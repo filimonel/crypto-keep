1651a60b25aad369ec3166559a7f5807fcc02bee
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-04-13 14:22:11 +1000

    created controllers, actions and views

---
+++ app/controllers/cryptos_controller.rb
@@ -0,0 +1,13 @@
+class CryptosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'portfolios/index'
+  get 'portfolios/show'
+  get 'portfolios/new'
+  get 'portfolios/edit'
+  get 'cryptos/index'
+  get 'cryptos/show'
+  get 'cryptos/new'
+  get 'cryptos/edit'
   devise_for :users
   resources :cryptos
   resource :portfolio
