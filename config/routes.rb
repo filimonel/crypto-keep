8a6e08f1b478393326409225bcf24ba2e84f2afe
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-04-14 20:26:05 +1000

    removed un-needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'portfolios/index'
-  get 'portfolios/show'
-  get 'portfolios/new'
-  get 'portfolios/edit'
-  get 'cryptos/index'
-  get 'cryptos/show'
-  get 'cryptos/new'
-  get 'cryptos/edit'
   devise_for :users
+  
   resources :cryptos
   resource :portfolio
   
