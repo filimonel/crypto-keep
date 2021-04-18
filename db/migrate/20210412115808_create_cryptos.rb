c164924d055205f10b8094ed1d556c8caf18b490
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-04-18 23:00:57 +1000

    Enabled the json data to be initialised into crypto models when seeds file is run. Crypto data is now available - just need to set it up so it scrapes periodically

--- db/migrate/20210412115808_create_cryptos.rb
+++ db/migrate/20210412115808_create_cryptos.rb
@@ -3,11 +3,11 @@ class CreateCryptos < ActiveRecord::Migration[6.1]
     create_table :cryptos do |t|
       t.integer :cmc_rank
       t.string :name
-      t.float :price
-      t.integer :volume_24h
-      t.integer :market_cap
-      t.integer :circulating_supply
-      t.integer :max_supply
+      t.integer :price
+      t.bigint :volume_24h
+      t.bigint :market_cap
+      t.bigint :circulating_supply
+      t.bigint :max_supply
       
       t.timestamps
     end
