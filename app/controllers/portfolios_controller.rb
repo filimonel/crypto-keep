05d6287c560cf5728a65cc0c08bd58fc7afe6a62
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-04-18 21:24:06 +1000

    tested associations between models. User now have individual portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,13 +1,9 @@
 class PortfoliosController < ApplicationController
   def index
+    
   end
 
   def show
   end
 
-  def new
-  end
-
-  def edit
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :portfolios, dependent: :destroy
+  has_many :cryptos, through: :portfolios
 end
